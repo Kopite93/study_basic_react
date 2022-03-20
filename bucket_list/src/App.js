b6a379f24f3954f4d9887b4e4f628222b294d169
Kopite93 <1000nulee@naver.com>
2022-03-20 23:55:58 +0900

    220320

--- bucket_list/src/App.js
+++ bucket_list/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./style.css";
 import BucketList from "./BucketList";
 import styled from "styled-components";
 
@@ -10,34 +9,71 @@ class App extends React.Component {
     this.state = {
       list: ["영화관 가기", "매일 책읽기", "수영 배우기"],
     };
+
+    this.text = React.createRef();
+  }
+
+  componentDidMount() {
+    console.log(this.text);
   }
 
   render() {
     return (
       <div className="App">
-        <MyStyled>
+        {/* <MyStyled>
           <p>I'm studying.</p>
-        </MyStyled>
-        {/* <div className="container">
-          <h1>내 버킷리스트</h1>
-          <hr className="line"/>
+        </MyStyled> */}
+
+        <Container>
+          <Title>내 버킷리스트</Title>
+          <Line />
           <BucketList list_a={this.state.list} />
-        </div> */}
+        </Container>
+
+        <div>
+          <input
+            type="text"
+            ref={this.text}
+            onChange={() => {
+              console.log(this.text.current.value);
+            }}
+          />
+        </div>
       </div>
     );
   }
 }
 
-const MyStyled = styled.div`
+// const MyStyled = styled.div`
+//   width: 50vw;
+//   height: 150px;
+//   background-color: ${(props) => (props.bg_color ? "lightgray" : "skyblue")};
+//   p {
+//     color: #fff;
+//   }
+//   &:hover {
+//     background-color: lightgray;
+//   }
+// `;
+
+const Container = styled.div`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  background-color: #fff;
   width: 50vw;
-  height: 150px;
-  background-color: ${(props) => (props.bg_color ? "lightgray" : "skyblue")};
-  p {
-    color: #fff;
-  }
-  &:hover {
-    background-color: lightgray;
-  }
+  max-width: 350px;
+  height: 80vh;
+  margin: auto;
+  padding: 16px;
+`;
+
+const Title = styled.h1`
+  color: skyblue;
+  text-align: center;
+`;
+
+const Line = styled.hr`
+  margin: 16px 0px;
 `;
 
 export default App;
--- bucket_list/src/BucketList.js
+++ bucket_list/src/BucketList.js
@@ -1,17 +1,26 @@
 import React from "react";
+import styled from "styled-components";
 
 const BucketList = ({ list_a }) => {
   console.log(list_a);
-  const my_lists = list_a;
+  const my_lists = list_a
+  const my_wrap = React.useRef(null);
+  console.log(my_wrap)
 
   return (
-    <div>
+    <div ref={my_wrap}>
       {my_lists.map((list, index) => {
         console.log(list);
-        return <div className="list-item" key={index}>{list}</div>;
+        return <Itemstyle key={index}>{list}</Itemstyle>;
       })}
     </div>
   );
 };
+
+const Itemstyle = styled.div`
+  padding: 16px;
+  margin: 8px;
+  background-color: lightblue;
+`;
 
 export default BucketList;
