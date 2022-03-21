0b0e0ab9cc0eefe3526b77ef6d2c5ee5a02b4db8
Kopite93 <1000nulee@naver.com>
2022-03-22 01:11:24 +0900

    220321

---
+++ day_grade/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import "./style.css";
+import React from "react";
+import Detail from "./Detail";
+import { Route } from "react-router-dom";
+import Main from "./Main";
+
+function App() {
+  const day = ["일", "월", "화", "수", "목", "금", "토"];
+
+  return (
+    <div className="App">
+      <Route path="/" exact render={(props) => <Main day={day} />} />
+      <Route path="/review" component={Detail} />
+    </div>
+  );
+}
+
+export default App;
---
+++ day_grade/src/Main.js
@@ -0,0 +1,50 @@
+import React from "react";
+import Detail from "./Detail";
+import { Route, useHistory } from "react-router-dom";
+
+const Main = (props) => {
+  const day = props.day;
+  const circle_count = Array.from({ length: 5 }, (v, i) => i);
+
+  const week_rates = day.map((day, idx) => {
+    return {
+      day: day,
+      rate:
+        Math.floor(Math.random() * (Math.floor(5) - Math.ceil(1) + 1)) +
+        Math.ceil(1),
+    };
+  });
+  console.log(week_rates);
+  const history = useHistory();
+  return (
+    <div className="Container">
+      <h2>내 일주일은?</h2>
+      {week_rates.map((day, i) => {
+        return (
+          <div className="dayName" key={i}>
+            <h4>{day.day}</h4>
+            {circle_count.map((cur, idx) => {
+              return (
+                <div
+                  className="Circle"
+                  key={idx}
+                  style={{
+                    backgroundColor: day.rate > idx ? "skyblue" : "#ddd",
+                  }}
+                ></div>
+              );
+            })}
+            <div
+              className="goDetail"
+              onClick={() => {
+                history.push(`/review/${day.day}`);
+              }}
+            ></div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Main;
